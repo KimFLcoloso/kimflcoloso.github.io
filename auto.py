4399dd8c2e009c078884341d702cb86f0ad0e35f
KimFLcoloso <160126164+KimFLcoloso@users.noreply.github.com>
2024-03-05 08:54:22 +0900

    Update auto.py

--- auto.py
+++ auto.py
@@ -1,8 +1,14 @@
 # generate_md.py
-md_content = """# 제목
+md_content = """---
+published: true
+layout: post
+title: "자동 테스트"
+---
+
+## h2
 이곳에 마크다운 내용을 작성합니다.
 """
 
 # `.md` 파일을 posts 폴더에 저장합니다.
-with open('posts/generated_post.md', 'w') as file:
+with open('_posts/2024-03-05-post.md', 'w') as file:
     file.write(md_content)
